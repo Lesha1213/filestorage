ffcd7ccc47457c5fb5748c34f55382febd578081
Dmitriy Bushin <dbushin@asteriosoft.com>
2020-08-31 04:00:34 +0700

    Add migration

---
+++ src/migrations/m200617_215405_create__files__structure.php
@@ -0,0 +1,128 @@
+<?php
+
+declare(strict_types=1);
+
+namespace reactivestudio\filestorage\migrations;
+
+use yii\db\Migration;
+
+/**
+ * Migration to create a table structure {{%files}}
+ * @package reactivestudio\filestorage\migrations
+ */
+class m200617_215405_create__files__structure extends Migration
+{
+    /**
+     * Table name
+     * @var string
+     */
+    private $table = '{{%files}}';
+
+    /**
+     * Base encoding
+     * @var string
+     */
+    private $character = 'utf8mb4';
+
+    /**
+     * Collate
+     * @var string
+     */
+    private $collate = 'utf8mb4_unicode_ci';
+
+    /**
+     * {@inheritDoc}
+     */
+    public function safeUp()
+    {
+        /** Create table */
+        $this->createTable(
+            $this->table,
+            [
+                'id' => $this
+                    ->integer()
+                    ->notNull()
+                    ->append('AUTO_INCREMENT PRIMARY KEY')
+                    ->comment('File Identifier'),
+                'group' => $this
+                    ->string(100)
+                    ->notNull()
+                    ->comment('Group name of files'),
+                'related_entity_id' => $this
+                    ->integer()->
+                    null()
+                    ->comment('Related entity identifier'),
+                'original_name' => $this
+                    ->string(255)
+                    ->notNull()
+                    ->comment('Original file name'),
+                'original_extension' => $this
+                    ->string(16)
+                    ->notNull()
+                    ->comment('Original file extension'),
+                'system_name' => $this
+                    ->string(255)
+                    ->notNull()
+                    ->unique()
+                    ->comment('System file name, including extension'),
+                'display_name' => $this
+                    ->string(255)
+                    ->notNull()
+                    ->comment('Display file name, including extension'),
+                'mime' => $this
+                    ->string(255)
+                    ->notNull()
+                    ->comment('MIME-type of the file'),
+                'size' => $this
+                    ->integer()
+                    ->notNull()
+                    ->unsigned()
+                    ->defaultValue(0)
+                    ->comment('File size in bytes'),
+                'created_at' => $this
+                    ->integer()
+                    ->notNull()
+                    ->comment('Date and time of creating file, in unix timestamp format'),
+                'updated_at' => $this
+                    ->integer()
+                    ->notNull()
+                    ->comment('Date and time of updating file, in unix timestamp format'),
+            ],
+            $this->options()
+        );
+
+        /** Add comment to table */
+        $this->addCommentOnTable($this->table, 'Files');
+
+        /** Add index for the field `group` */
+        $this->createIndex('{{%idx__' . 'files' . '__group}}', $this->table, 'group');
+
+        /** Add index for the field `related_entity_id` */
+        $this->createIndex('{{%idx__' . 'files' . '__related_entity_id}}', $this->table, 'related_entity_id');
+
+        /** Add index for the field `created_at` */
+        $this->createIndex('{{%idx__' . 'files' . '__created_at}}', $this->table, 'created_at');
+
+        /** Add index for the field `updated_at` */
+        $this->createIndex('{{%idx__' . 'files' . '__updated_at}}', $this->table, 'updated_at');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable($this->table);
+    }
+
+    /**
+     * Get table options
+     * @return string|null
+     */
+    protected function options(): ?string
+    {
+        return ('mysql' === $this->db->driverName)
+            ? 'CHARACTER SET ' . $this->character . ' COLLATE ' . $this->collate . ' ENGINE=InnoDB'
+            : null;
+    }
+}
